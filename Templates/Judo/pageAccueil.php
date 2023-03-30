98246526eee31a750c8c0437eae4137729a58e4b
Rijckaert Tom <200082@CCM-Asty-Moulin.local>
2023-03-30 14:31:01 +0200

    ajout de photo + essai css

--- Templates/Judo/pageAccueil.php
+++ Templates/Judo/pageAccueil.php
@@ -1,1 +1,7 @@
-<h1>Page d'accueil</h1>
+<h1 class="mb5">Judo</h1>
+<div class="FlexContainer wrap techniqueJudo">
+    <img class="block" src="../Images/technique1.png" alt="">
+    <img class="block" src="../Images/technique2.png" alt="">
+    <img class="block" src="../Images/technique3.png" alt="">
+    <img class="block" src="../Images/technique4.png" alt="">
+</div>
--- index.php
+++ index.php
@@ -17,6 +17,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Judo</title>
     <link rel="stylesheet" href="CSS/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Macondo&family=Poppins&display=swap" rel="stylesheet">
 </head>
 <body>
     <header id="menu">
